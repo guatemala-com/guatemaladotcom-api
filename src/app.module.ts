80b9d8ebd1d8c947c6878b44f6e239286f201421
Erick Pac <erick@guatemala.com>
2025-07-02 14:54:29 -0600

    Add Prisma module and service for database integration
    - Introduced PrismaModule to encapsulate PrismaService.
    - Updated app.module.ts to include PrismaModule in imports.
    - Implemented PrismaService with lifecycle hooks for connection management.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HealthCheckModule } from './modules/healthcheck/healthcheck.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HealthCheckModule],
+  imports: [ConfigModule.forRoot(), HealthCheckModule, PrismaModule],
   controllers: [],
   providers: [],
 })
